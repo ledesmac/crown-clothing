57934fda5bb56a28091309c4429e5c9e8298ca53
Alex Ledesma <alexledesma@Alexs-MBP.domain>
2021-03-19 17:47:34 -0600

    adding header component and links to home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './App.scss';
 import Homepage from './containers/Homepage/Homepage';
-import { Link, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import ShopPage from './containers/Shop/Shop';
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>);  
-}
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop' component= {ShopPage} />
-      
+    {/* Putting header outside of the switch allows
+        it to appear on every render */}
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component= {ShopPage} />
+      </Switch>
     </div>
   );
 }

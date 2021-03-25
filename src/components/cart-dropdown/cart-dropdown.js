e7e95eae5443c1046f175a03dbccd0c1af6b8e3a
Alex Ledesma <alexledesma@Alexs-MBP.domain>
2021-03-24 22:02:59 -0600

    completed cart drop down with cart item compenent and connected to cart reducer

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { connect }  from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button';
-
+import CartItem from '../cart-item/cart-item';
 import './cart-dropdown.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return(
         <div className='cart-dropdown'>
             <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => {
+                        return(
+                            <CartItem 
+                                key={cartItem.id} 
+                                item={cartItem} />
+                        );
+                    })
+                }
             </div>
             <CustomButton>
                 GO TO CHECKOUT
             </CustomButton>
         </div>
     );
-}
+};
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => {
+    return({
+        cartItems
+    });  
+};
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -12,11 +12,11 @@ const CollectionPreview = ({title, items}) => {
                 {
                     items
                         .filter((item, idx) => idx < 4)
-                        .map(({id, ...otherItemProps}) => {
+                        .map((item) => {
                         return(
                             <CollectionItem
-                                key={id} 
-                                {...otherItemProps}/>
+                                key={item.id} 
+                                item={item}/>
                         )
                     })
                 }
